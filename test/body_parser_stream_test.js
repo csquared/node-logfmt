3f6b1001773c47024a098ffa0246513cfc60398d
Chris Continanza <christopher.continanza@gmail.com>
2013-07-30 11:09:53 -0700

    streaming body parser test

---
+++ test/body_parser_stream_test.js
@@ -0,0 +1,105 @@
+var logfmt = require('../logfmt'),
+    stream = require('stream'),
+    assert = require('assert');
+
+test("stream body parser skips parsing when req._body is true", function(){
+
+  var mockReq = new stream.Readable;
+  mockReq.get = function(){
+    return 'application/logplex-1';
+  }
+  mockReq._read = function(){};
+  mockReq.push('hello=kitty');
+  mockReq.push(null);
+
+  mockReq._body = true;
+
+  var next = function(err){
+    assert.equal(mockReq.body, undefined)
+  };
+
+  var parser = logfmt.bodyParserStream();
+  parser(mockReq, null, next)
+})
+
+test("stream body parser skips parsing when contentType does not match", function(){
+  var mockReq = new stream.Readable;
+  mockReq.get = function(){
+    return 'application/foo';
+  }
+  mockReq._read = function(){};
+  mockReq.push('hello=kitty');
+  mockReq.push(null);
+
+  var next = function(err){
+    assert.equal(mockReq.body, undefined)
+  };
+
+  var parser = logfmt.bodyParserStream();
+  parser(mockReq, null, next)
+
+})
+
+test("stream body parser converts body lines to object read stream", function(done){
+  var mockReq = new stream.Readable;
+  mockReq.get = function(){
+    return 'application/logplex-1';
+  }
+  mockReq._read = function(){};
+  mockReq.push('hello=kitty');
+  mockReq.push(null);
+  var next = function(){};
+
+  var parser = logfmt.bodyParserStream();
+  parser(mockReq, null, next)
+
+  mockReq.body.on('readable', function(){
+    var data = mockReq.body.read();
+    assert.deepEqual(data, {hello: 'kitty'})
+    done();
+  })
+})
+
+
+test("stream body parser accepts contentType option", function(done){
+  var mockReq = new stream.Readable;
+  mockReq.get = function(){
+    return 'foo';
+  }
+  mockReq._read = function(){};
+  mockReq.push('hello=kitty');
+  mockReq.push(null);
+  var next = function(){};
+
+  var parser = logfmt.bodyParserStream({contentType: 'foo'});
+  parser(mockReq, null, next)
+
+  mockReq.body.on('readable', function(){
+    var data = mockReq.body.read();
+    assert.deepEqual(data, {hello: 'kitty'})
+    done();
+  })
+})
+
+test("body parser parses all the lines", function(done){
+  var mockReq = new stream.Readable;
+  mockReq.get = function(){
+    return 'application/logplex-1';
+  }
+  mockReq._read = function(){};
+  mockReq.push('hello=kitty\n');
+  mockReq.push('foo=bar\n');
+  mockReq.push('path=/');
+  mockReq.push(null);
+  var next = function(){};
+
+  var parser = logfmt.bodyParserStream();
+  parser(mockReq, null, next)
+
+  var matches = [{path: '/'}, {foo: 'bar'}, {hello: 'kitty'}];
+  mockReq.body.on('readable', function(){
+    var data = mockReq.body.read();
+    assert.deepEqual(data, matches.pop())
+    if(matches.length == 0) done();
+  })
+})
