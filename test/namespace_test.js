02269ce7f68cb9f767308a5f57fce1f2316654a5
Chris Continanza <christopher.continanza@gmail.com>
2013-08-17 19:42:48 -0700

    it works

--- test/namespace_test.js
+++ test/namespace_test.js
@@ -14,15 +14,47 @@ suite('logfmt.namespace', function() {
     assert.deepEqual(data, recovered);
   })
 
+  test("does not modify data passed in", function(){
+    var logfmt2 = logfmt.namespace({ns: 'logfmt'});
+    var mock_sink = new OutStream;
+    var data = {foo: 'bar', a: 14}
+    logfmt2.log(data, mock_sink);
+    assert.deepEqual(data, {foo: 'bar', a: 14});
+  })
+
   test("includes data passed in on all log lines", function(){
     var logfmt2 = logfmt.namespace({ns: 'logfmt'});
     var mock_sink = new OutStream;
     var data = {foo: 'bar', a: 14}
     logfmt2.log(data, mock_sink);
-    assert.equal("foo=bar a=14 ns=logfmt\n", mock_sink.logline)
+    assert.equal("ns=logfmt foo=bar a=14\n", mock_sink.logline)
     logfmt2.log({}, mock_sink);
     assert.equal("ns=logfmt\n", mock_sink.logline)
     logfmt2.log(data, mock_sink);
-    assert.equal("foo=bar a=14 ns=logfmt\n", mock_sink.logline)
+    assert.equal("ns=logfmt foo=bar a=14\n", mock_sink.logline)
+  })
+
+  test("works with logfmt.time", function(done){
+    var logfmt2 = logfmt.namespace({ns: 'logfmt'})
+    logfmt2.time(function(logger){
+      var mock_sink = new OutStream;
+      logger.log({}, mock_sink);
+      var actual = mock_sink.logline;
+      assert(/^ns=logfmt elapsed=\dms\n$/.test(actual), actual)
+      done();
+    })
+  })
+
+  test("can chain namespace calls", function(done){
+    var logfmt2 = logfmt.namespace({ns: 'logfmt'})
+                        .namespace({thing: 'data'})
+
+    logfmt2.time(function(logger){
+      var mock_sink = new OutStream;
+      logger.log({}, mock_sink);
+      var actual = mock_sink.logline;
+      assert(/^ns=logfmt thing=data elapsed=\dms\n$/.test(actual), actual)
+      done();
+    })
   })
 })
