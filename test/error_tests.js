29313870f60fd34f887f2f63f0bff13b8ee2e83b
Chris Continanza <christopher.continanza@gmail.com>
2013-08-29 09:53:56 -0700

    make it error=true

--- test/error_tests.js
+++ test/error_tests.js
@@ -9,8 +9,8 @@ suite('logfmt.error', function() {
     logfmt.stream = new(require('./outstream'));
     logfmt.error(err);
     var id = logfmt.stream.lines[0].match(/id=(\d+)/)[1];
-    assert.equal(logfmt.stream.lines[0], 'at=error id=' + id + ' message=testing\n');
-    assert.equal(logfmt.stream.lines[1], 'at=error id=' + id + ' line=0 trace="Error: testing"\n');
+    assert.equal(logfmt.stream.lines[0], 'error=true id=' + id + ' message=testing\n');
+    assert.equal(logfmt.stream.lines[1], 'error=true id=' + id + ' line=0 trace="Error: testing"\n');
   });
 
   test('sends only a max number of log lines', function() {
