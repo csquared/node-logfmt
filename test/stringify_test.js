969826c234a45cc07b391c7f786ec65bb4882c3f
Chris Continanza <christopher.continanza@gmail.com>
2014-03-17 22:01:13 -0700

    move tests to stringify test

--- test/stringify_test.js
+++ test/stringify_test.js
@@ -3,13 +3,45 @@ var logfmt = require('../logfmt'),
 
 suite('logfmt.stringify', function() {
 
-  test("stringify object with properties", function(){
+  test("simple key value pairs", function(){
+    var data = {foo: 'bar', a: 14}
+    assert.equal("foo=bar a=14", logfmt.stringify(data))
+  })
 
+  test("true and false", function(){
+    var data = {foo: true, bar: false}
+    assert.equal("foo=true bar=false", logfmt.stringify(data))
+  })
+
+  test("quotes strings with spaces in them", function(){
+    var data = {foo: "hello kitty"}
+    assert.equal("foo=\"hello kitty\"", logfmt.stringify(data))
+  })
+
+  test("quotes strings with equals in them", function(){
+    var data = {foo: "hello=kitty"}
+    assert.equal("foo=\"hello=kitty\"", logfmt.stringify(data))
+  })
+
+  test("escapes quotes within strings with spaces in them", function(){
+    var data = {foo: 'hello my "friend"'}
+    assert.equal('foo="hello my \\"friend\\""', logfmt.stringify(data))
+    var data = {foo: 'hello my "friend" whom I "love"'}
+    assert.equal('foo="hello my \\"friend\\" whom I \\"love\\""', logfmt.stringify(data))
+  })
+
+  test("undefined is logged as nothing", function(){
+    var data = {foo: undefined}
+    assert.equal("foo=", logfmt.stringify(data))
+  })
+
+  test("null is logged as nothing", function(){
+    var data = {foo: null}
+    assert.equal("foo=", logfmt.stringify(data))
   })
 
   test("stringify object with properties", function(){
-    var data = {}
-    data.foo = 'bar';
+    var data = {foo: 'bar'}
     assert.equal('foo=bar', logfmt.stringify(data));
   })
 
@@ -24,12 +56,4 @@ suite('logfmt.stringify', function() {
     var data = {foo: true}
     assert.equal('foo=true', logfmt.stringify(data));
   })
-
-  /*
-  test("stringify annoying strings", function(){
-    var data = {foo: '\"'}
-    console.log(logfmt.stringify(data));
-    //assert.equal('foo=true',
-  })
-  */
 })
